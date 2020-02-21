93cfe4269fd302bf0c0cda15ae803e0f0a55fa3d
Warren Harper II <warrenharper2@att.net>
2020-02-21 01:05:21 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,12 +6,12 @@ class App < Sinatra::Base
     get '/' do
           erb :'../views/super_hero'
         end
-    
+
         post '/teams' do
           @team_name = params["team"]["name"]
           @team_motto = params["team"]["motto"]
           @team_members = params["team"]["members"]
-    
+
           erb :'../views/team'
         end
 
